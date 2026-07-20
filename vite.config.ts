59f3b2dff70fbd4317a2b2063402f7e12183d890
grace-bidos <tadashi240312@gmail.com>
2026-07-20 17:53:24 +0900

    chore: Vueのbuild toolingをadd

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import tailwindcss from "@tailwindcss/vite";
+import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [vue(), tailwindcss()],
   server: {
     strictPort: true,
   },
